13d27a383bacfb367fbb9b0ae86ac1c759146db5
Lucas Freire <lucasfreiredev1@gmail.com>
2022-10-27 20:54:49 -0300

    Asterisks with system.out.print

---
+++ Asterisks/src/Asterisks.java
@@ -0,0 +1,16 @@
+
+public class Asterisks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String first = "* * * * * * * *";
+		String second = " * * * * * * * *";
+		
+		System.out.println("TABULEIRO DAMAS ASTERISCOS EX BOOK");
+		for (int i = 1; i <= 4; i++) {
+			System.out.println(first);
+			System.out.println(second);
+		}
+	}
+
+}
