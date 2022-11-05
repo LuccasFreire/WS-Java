69b4eb7e6c304f03890ae6f8688dfd5d6ac80356
Lucas Freire <lucasfreiredev1@gmail.com>
2022-11-04 23:26:12 -0300

    Updated the main method

--- BankAccountOOP/src/application/BankAccountEx.java
+++ BankAccountOOP/src/application/BankAccountEx.java
@@ -6,6 +6,7 @@ public class BankAccountEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Account account1;
 		Scanner kb = new Scanner(System.in);
 		System.out.print("Enter account number: ");
 		int accountNum = kb.nextInt();
@@ -13,30 +14,29 @@ public class BankAccountEx {
 		System.out.print("Enter account holder: ");
 		String accountHolder = kb.next();
 		
+		String logical = kb.nextLine();
 		System.out.println("Is there an initial deposit (Y/N)? ");
-		String logical = kb.next().toLowerCase();
+		logical = kb.next().toLowerCase();
 		
 		
 		if (logical != "n") {
 			System.out.println("Enter initial deposit: ");
 			double deposit = kb.nextDouble();
-			Account account1 = new Account(accountNum, accountHolder, deposit);
-			account1.makeDeposit();
+			account1 = new Account(accountNum, accountHolder, deposit);
 		}
 		else {
-			Account account1 = new Account(accountNum, accountHolder);
-			account1.makeDeposit();
+			account1 = new Account(accountNum, accountHolder);
 		}
 		
-		System.out.println("Enter deposit value: ");
-		double amount = kb.nextDouble();
-		Account account1 = new Account(amount);
-		account1.makeDeposit();
+//		System.out.println("Enter deposit value: ");
+//		double deposit = kb.nextDouble();
+//		Account account1 = new Account(deposit);
+//		account1.makeDeposit(deposit);
 		
 		System.out.println(account1.toString());
 		
 		System.out.println("Enter a withdraw value: ");
-		amount = kb.nextDouble();
+		double amount = kb.nextDouble();
 		account1.makeWithdraw(amount);
 
 		System.out.println(account1.toString());
--- BankAccountOOP/src/entities/Account.java
+++ BankAccountOOP/src/entities/Account.java
@@ -9,7 +9,7 @@ public class Account {
 	public Account(int accountNum, String accountHolder, double amount) {
 		this.accountNum = accountNum;
 		this.accountHolder = accountHolder;
-		this.amount = amount;
+		makeDeposit(amount);
 	}
 	public Account(int accountNum, String accountHolder) {
 		this.accountNum = accountNum;
@@ -23,9 +23,6 @@ public class Account {
 		return "Account " + getAccountNum() + " Holder: " + getAccountHolder() + " Balance: " + String.format("%.2f",getBalance());
 	}
 	
-	public void setAccountNum(int num) {
-		this.accountNum = num;
-	}
 	public int getAccountNum() {
 		return accountNum;
 	}
@@ -35,19 +32,16 @@ public class Account {
 	public String getAccountHolder() {
 		return accountHolder;
 	}
-	public void setAmount(double price) {
-		this.amount = price;
-	}
 	public double getAmount() {
 		return amount;
 	}
 	public double getBalance() {
 		return balance;
 	}
-	public void makeDeposit() {
+	public void makeDeposit(double amount) {
 		this.balance += amount;
 	}
-	public void makeWithdraw(double amount) {
-		this.balance -= amount;
+	public void makeWithdraw(double price) {
+		this.balance -= (price) + 5.0;
 	}
 }
