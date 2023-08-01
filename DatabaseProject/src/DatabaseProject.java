52a1724db293b6022dfb804c21a3b6fc8ee0fb64
Lucas Freire <lucasfreiredev1@gmail.com>
2023-07-31 23:40:16 -0300

    Created data read from table

--- DatabaseProject/src/DatabaseProject.java
+++ DatabaseProject/src/DatabaseProject.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseProject {
 
@@ -14,6 +16,15 @@ public class DatabaseProject {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");  
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb", "root", "");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM USER");
+			while(rs.next()) {
+				String name = rs.getString("username");
+				Integer passw = rs.getInt("password");
+				String email = rs.getString("email");
+				String phone = rs.getString("phone");
+				System.out.printf("NAME: %s, PASSWORD: %d, EMAIL: %s, PHONE: %s\n",name, passw, email, phone);
+			}
 			System.out.println("Database success");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
